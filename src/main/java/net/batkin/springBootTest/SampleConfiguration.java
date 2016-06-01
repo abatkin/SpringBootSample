00afe5bc2f6bac4d87fef1e0c991898dbde8198c
Adam Batkin <adam@batkin.net>
2016-05-31 22:40:42 -0400

    Use @Import instead of @EnableAutoConfiguration

--- src/main/java/net/batkin/springBootTest/SampleConfiguration.java
+++ src/main/java/net/batkin/springBootTest/SampleConfiguration.java
@@ -1,12 +1,38 @@
 package net.batkin.springBootTest;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
+import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslationAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
-@EnableAutoConfiguration
 @ComponentScan
+@Import({
+        ConfigurationPropertiesAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        DispatcherServletAutoConfiguration.class,
+        EmbeddedServletContainerAutoConfiguration.class,
+        ErrorMvcAutoConfiguration.class,
+        HttpEncodingAutoConfiguration.class,
+        HttpMessageConvertersAutoConfiguration.class,
+        JacksonAutoConfiguration.class,
+        JmxAutoConfiguration.class,
+        MultipartAutoConfiguration.class,
+        PersistenceExceptionTranslationAutoConfiguration.class,
+        PropertyPlaceholderAutoConfiguration.class,
+        ServerPropertiesAutoConfiguration.class,
+        TransactionAutoConfiguration.class,
+        WebMvcAutoConfiguration.class
+})
 public class SampleConfiguration {
 
 }
