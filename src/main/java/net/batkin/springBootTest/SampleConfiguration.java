291c5264233ebd7289f03be1365da5d8a6558fc3
Adam Batkin <adam@batkin.net>
2016-06-05 21:48:35 -0400

    enable transaction management

--- src/main/java/net/batkin/springBootTest/SampleConfiguration.java
+++ src/main/java/net/batkin/springBootTest/SampleConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.web.*;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan
@@ -33,6 +34,7 @@ import org.springframework.context.annotation.Import;
         TransactionAutoConfiguration.class,
         WebMvcAutoConfiguration.class
 })
+@EnableTransactionManagement
 public class SampleConfiguration {
 
 }
